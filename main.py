c5681bef82743fd064dc66b42d74b3367abfffdf
اسمك_على_جيت_هاب <_>
2026-05-18 21:25:06 +0100

    Enforce fully defined MDApp and fresh workflow structure

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-# تعريف الواجهة الأساسية
 class MainScreen(Screen):
     pass
 
-# تعريف كلاس التطبيق الرئيسي مع الاستدعاء الصحيح
 class SatChannelEditorPro(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Blue"
         
-        # إنشاء مدير الواجهات
         sm = ScreenManager()
         sm.add_widget(MainScreen(name='main'))
         return sm
